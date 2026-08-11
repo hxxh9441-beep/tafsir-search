24fbf01a28df10c5a93535bc4195a48d825b0d84
hxxh9441-beep <hxxh9441@gmail.com>
2026-08-11 18:40:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* سيرفر الخدمة — الباحث القرآني (Offline-First + تحديث فوري) */
-const CACHE = 'quran-search-v20';
+const CACHE = 'quran-search-v25';
 const APP_SHELL = [
   './',
   './index.html',
